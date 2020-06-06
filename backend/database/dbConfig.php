35f77f66699fc9d95603e797a760c4da835330c5
kosco <kristijan.koscak16@gmail.com>
2020-06-06 23:00:55 +0200

    Ready to upload.

---
+++ backend/database/dbConfig.php
@@ -0,0 +1,11 @@
+<?php
+    namespace database;
+
+    class dbConfig{
+        const servername= "localhost";//"eu-cdbr-west-03.cleardb.net";
+        const user="root";//"b9588159f8c891";
+        const password="";//"3f64437a";
+        const database="cfcdb";//"heroku_8d76ccdc410ef31";
+    }
+
+?>
