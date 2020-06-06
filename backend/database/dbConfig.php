5694ab9ff4f2d8b82a2e368170bf08f6959b0e71
kosco <kristijan.koscak16@gmail.com>
2020-06-06 23:04:16 +0200

    DBconfig changed.

--- backend/database/dbConfig.php
+++ backend/database/dbConfig.php
@@ -2,10 +2,10 @@
     namespace database;
 
     class dbConfig{
-        const servername= "localhost";//"eu-cdbr-west-03.cleardb.net";
-        const user="root";//"b9588159f8c891";
-        const password="";//"3f64437a";
-        const database="cfcdb";//"heroku_8d76ccdc410ef31";
+        const servername= "eu-cdbr-west-03.cleardb.net";
+        const user="b9588159f8c891";
+        const password="3f64437a";
+        const database="heroku_8d76ccdc410ef31";
     }
 
 ?>
